3b52760a9e69cfbe545ea5a38c640301932bb2d2
Constantine Isaev <matthew.risedale@gmail.com>
2017-03-27 00:23:44 +0300

    toolbox done

---
+++ dist/assets/js/toolbox.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const toolboxItems = document.querySelectorAll(".toolbox__item > a");
+
+for (var i = toolboxItems.length - 1; i>=0; i--) {
+  var toolboxItem = toolboxItems[i];
+  toolAction(toolboxItem);
+}
+
+function toolAction(toolboxItem) {
+  toolboxItem.addEventListener("click", function(e) {
+    e.preventDefault();
+    (this.parentElement.classList.contains("toolbox__item--active") === true) ? this.parentElement.classList.remove("toolbox__item--active") : this.parentElement.classList.add("toolbox__item--active");
+  });
+}
